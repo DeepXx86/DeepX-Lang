0ed99439086608e044408b6db82a2acb05214ff3
Pascal <167556769+DeepXx86@users.noreply.github.com>
2024-09-06 19:18:58 +0700

    Update Main.hs

--- DeepX/src/Main.hs
+++ DeepX/src/Main.hs
@@ -4,6 +4,7 @@ import System.Environment (getArgs)
 import System.IO (readFile)
 import Text.Parsec (parse)
 import qualified Data.Map as Map
+import System.Process (callCommand) 
 
 import Parser
 import Evaluator
@@ -12,11 +13,20 @@ import AST
 main :: IO ()
 main = do
     args <- getArgs
-    let filename = head args
-    content <- readFile filename
-    let parsed = parse program filename content
-    case parsed of
-        Left err -> print err
-        Right exprs -> do
-            (result, _) <- runEval (evalBlock exprs) Map.empty
-            return ()
+    case args of
+
+        [] -> putStrLn "Usage: depth <filename>"
+        ("depth":filename:_) -> do
+            let command = "stack exec DeepX-exe \"" ++ filename ++ "\""
+            putStrLn $ "Executing: " ++ command
+            callCommand command
+
+        (filename:_) -> do
+            putStrLn $ "Running file: " ++ filename
+            content <- readFile filename
+            let parsed = parse program filename content
+            case parsed of
+                Left err -> print err
+                Right exprs -> do
+                    (result, _) <- runEval (evalBlock exprs) Map.empty
+                    return ()
